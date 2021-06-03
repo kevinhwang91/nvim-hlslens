aad0ff933c5f0c0ae6f030e14f51c176a64a3bd2
kevinhwang91 <kevin.hwang@live.com>
2021-06-03 17:57:32 +0800

    perf(position): improve performance while moving cursor

--- lua/hlslens/position.lua
+++ lua/hlslens/position.lua
@@ -78,7 +78,11 @@ function M.nearest_idx_info(plist, pattern)
                         end
                     end
                     i_pos_e = fn.searchpos(pattern, 'cen')
-                    fn.winrestview(wv)
+                    if wv.topline == fn.line('w0') then
+                        fn.cursor(wv.lnum, wv.col + 1)
+                    else
+                        fn.winrestview(wv)
+                    end
                 end
                 tbl.pos_e = i_pos_e
                 return tbl.pos_e
