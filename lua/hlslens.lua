0bd7ddb0aecf095055e9cb6eef0c25c70fb69cfc
kevinhwang91 <kevin.hwang@live.com>
2024-11-16 22:43:43 +0800

    feat!: remove warning about setup (#73)

--- lua/hlslens.lua
+++ lua/hlslens.lua
@@ -61,17 +61,7 @@ function M.nNPeekWithUFO(char, ...)
     return require('hlslens.ext.ufo'):nN(char, ...)
 end
 
-function M.setup(opts, warnFlag)
-    if not opts and warnFlag then
-        vim.schedule(function()
-            if not initialized then
-                vim.notify([[nvim-hlslens need to invoke `require('hlslens').setup()` to boot!]],
-                           vim.log.levels.WARN)
-            end
-            M.setup()
-        end)
-        return
-    end
+function M.setup(opts)
     if initialized and (not M._config or not opts) then
         return
     end
--- plugin/hlslens.vim
+++ plugin/hlslens.vim
@@ -9,5 +9,3 @@ if !has('nvim-0.6.1')
 endif
 
 let g:loaded_nvim_hlslens = 1
-
-lua vim.schedule(function() require('hlslens').setup(nil, true) end)
